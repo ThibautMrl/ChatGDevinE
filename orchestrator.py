2ba35cb6c6f83f84ae36b53dd0c0299c54760a1b
aygalic <aygalic@hotmail.fr>
2025-09-03 18:41:43 +0200

    feat: orchestrator

---
+++ orchestrator.py
@@ -0,0 +1,33 @@
+import gradio as gr 
+from typing import List, Optional
+from PIL import Image
+
+class Orchestrator():
+    def __init__(self):
+        self.wiki_url : Optional[str]
+        self.article : Optional[str]
+        self.title : Optional[str]
+        self.image : Optional[str]
+
+
+    def chose_article(self):
+        raise NotImplementedError
+
+    def get_article(self) -> str:
+        raise NotImplementedError
+
+    def get_image(self) -> Image:
+        raise NotImplementedError
+    
+    def get_title(self) -> str:
+        raise NotImplementedError
+
+    def _test_response(self, history : List[gr.ChatMessage]) -> bool:
+        if self.title in history[-1].content:
+            return True
+        else:
+            return False
+        
+    def get_response_from_model(self, history : List[gr.ChatMessage]) -> str:
+        self._test_response(history)
+        raise NotImplementedError
