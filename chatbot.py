11ee241bedaab4e9529aa27624604fe3db3001e7
Thibaut <t.maurel@dna-blast.com>
2025-09-03 18:12:16 +0200

    add new game boutton

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,30 @@
 import gradio as gr
 from PIL import Image
 
+history = [gr.ChatMessage(role="assistant", content="De quelle page Wikipedia provient cette image ?"),]
+img = Image.open(r"./Mbappe.jpg")
+
+def new_game():
+    history = [gr.ChatMessage(role="assistant", content="De quelle page Wikipedia provient cette image ?"), ]
+    img = Image.open(r"./Zidane.jpg")
+    return history,gr.Image(img,width=300, height=200)
+
 def chatbot_response(history, message):
     # Réponse texte + image
-    history.append((message, "Ceci est une réponse."))
+    history.append(gr.ChatMessage(role="user",content=message)),
+    history.append(gr.ChatMessage(role="assistant", content="Ceci est une réponse."))
     return history, ""
 
 with gr.Blocks() as demo:
-    img = Image.open(r"./Mbappe.jpg")
+
     image = gr.Image(img,width=300, height=200)
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.Button("Clear")
+    chatbot = gr.Chatbot(
+        history,type="messages"
+    )
+    msg = gr.Textbox(label="",placeholder="Entrez votre question ou réponse...")
+    clear = gr.Button("Nouvelle partie")
 
     msg.submit(chatbot_response, [chatbot, msg], [chatbot, msg])
-    clear.click(lambda: None, None, chatbot, queue=False)
+    clear.click(fn=new_game, outputs = [chatbot,image], queue=False)
 
 demo.launch()
