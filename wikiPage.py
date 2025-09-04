3820bec330b98edf94fe13e6be8c52456f9b2846
JulienBez <julienbezancon@gmail.com>
2025-09-04 14:47:31 +0200

    wikipage pus

--- wikiPage.py
+++ wikiPage.py
@@ -1,22 +1,60 @@
+import os
+import json
+import random
+import requests
 import wikipedia
+from tqdm import tqdm
 
-def get_random_wikipedia_page_with_image():
+def openJson(path):
+    "open a json file"
+    with open(path,'r',encoding='utf-8') as f:
+        data = json.load(f)
+    return data
 
-    wikipedia.set_lang("en")
-    random_title = wikipedia.random()
-    page = wikipedia.page(random_title)
+  
+def writeJson(path,data):
+    "create a json file"
+    with open(path,"w",encoding='utf-8') as f:
+        json.dump(data,f,indent=4,ensure_ascii=False)
 
-    while len(page.images) == 0:
-        random_title = wikipedia.random()
-        print("Random title:", random_title)
-        page = wikipedia.page(random_title)
 
+def getPage(title):
+    "return the page content of an article in a dedicated wikipedia object"
+    page = wikipedia.page(title,auto_suggest=False)
     return page
 
 
-#page = get_random_wikipedia_page_with_image()
-#
-#title = page.title
-#summary = page.summary
-#content = page.content.strip()
-#image = page.images[0]
+def filterArticles(articles):
+    "keep only articles with an image and a content"
+    kept_articles = [] #with images
+    for article in tqdm(articles):
+        try:
+            a = getPage(article["article"])
+            entry = {
+                "title":a.title,
+                #"summary":a.summary,
+                "content":a.content.strip(),
+                "image":a.images[0]
+            }
+            if len(a.images) != 0 and len(a.content) != 0:
+                kept_articles.append(entry)
+        except:
+            continue
+    return kept_articles
+
+def getTopViews(project="en.wikipedia.org", access="all-access", year="2025", month="08", limit=50):
+    "saves in a json file the N articles with the most views in wikipedia"
+    url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/{project}/{access}/{year}/{month}/all-days"
+    r = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (platform; rv:gecko-version) Gecko/gecko-trail Firefox/firefox-version"})
+    r.raise_for_status()
+    data = r.json()
+    articles = data["items"][0]["articles"][:limit]
+    articles = filterArticles(articles)
+    writeJson(f"articles.json",articles)
+
+
+if not os.path.exists("articles.json"):
+    getTopViews(limit=150)
+
+random_article = random.choice(openJson(f"articles.json"))
+print(random_article["title"])
