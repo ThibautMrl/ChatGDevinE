ad0923fcad38a74d6d44b4897ee57586e76b2fec
aygalic <aygalic@hotmail.fr>
2025-09-04 10:28:14 +0200

    chore: cleanup

--- llm_interface.py
+++ llm_interface.py
@@ -1,8 +1,6 @@
 from ollama import Client
 import gradio as gr
 
-CORRECT_ANS = "mbappe"
-
 class Chat:
     def __init__(self):
         #self.conversation_starter = {
@@ -34,11 +32,10 @@ class Chat:
 
     def get_response_from_model(self, history: list[gr.ChatMessage]):
         formated_hist = self._format_gr_hist(history)
-        # Stream response so you see it as it generates
         chat_response = self.client.chat(
             model=self.model_name,
             messages=formated_hist,
             stream=False,
         )
         content = chat_response["message"]["content"]
-        return content #self._format_msg_to_gr(content)
+        return content
